8180e33960ee3f6239e12431fa4f94a123c9eb25
bobur-96 <bovlogs96@gmail.com>
2025-01-10 00:21:17 +0900

    feat: Apollo client connection & modify Mobile version

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,15 +6,19 @@ import { useState } from 'react'
 import '../scss/app.scss'
 import '../scss/pc/main.scss'
 import "../scss/mobile/main.scss"
+import { ApolloProvider } from "@apollo/client";
+import client from "@/apollo/client";
 
 export default function App({ Component, pageProps }: AppProps) {
 	//@ts-ignore
 	const [theme, setTheme] = useState(createTheme(light))
 	//Socket.io
 	return (
-		<ThemeProvider theme={theme}>
-			<CssBaseline />
-			<Component {...pageProps} />
-		</ThemeProvider>
+		<ApolloProvider client={client}>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</ApolloProvider>
 	)
 }
